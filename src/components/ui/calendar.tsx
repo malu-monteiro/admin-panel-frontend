e8fb5df40492eb4cbaf328d613fff083e9b83153
Maria <malumonteiro.dev@gmail.com>
2025-04-10 17:54:34 -0300

    feat: add new color for day selected on calendar

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -47,10 +47,10 @@ function Calendar({
         day_range_end:
           "day-range-end aria-selected:bg-primary aria-selected:text-primary-foreground",
         day_selected:
-          "bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-muted-foreground",
+          "bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-chart-4",
         day_today: "bg-accent text-accent-foreground",
         day_outside:
-          "day-outside text-muted-foreground aria-selected:text-muted-foreground",
+          "day-outside text-muted-foreground aria-selected:text-chart-4",
         day_disabled: "text-muted-foreground opacity-50",
         day_range_middle:
           "aria-selected:bg-accent aria-selected:text-accent-foreground",
