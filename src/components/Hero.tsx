982d93b5957fc102443abdc5cccb72e48bc3af2a
Maria <malumonteiro.dev@gmail.com>
2025-04-14 22:48:43 -0300

    feat: hero structure

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,44 +1,28 @@
 import { SchedulingButton } from "./SchedulingButton";
-import Cats from "../assets/dog-and-cat.png";
+import Pets from "../assets/pets.png";
 
 export function Hero() {
   return (
-    <section className="bg-yellow-300 py-16 px-6 md:px-24 flex flex-col md:flex-row items-center justify-center relative overflow-hidden">
-      {/* Texto principal */}
-      <div className="max-w-xl text-center md:text-left">
-        <h1 className="text-4xl md:text-5xl font-bold text-white mb-6">
-          We take good <br /> care of your pet
+    <section className="relative bg-noise to-amber-0 py-16 px-6 md:px-24 flex flex-col md:flex-row items-center justify-center overflow-hidden z-0">
+      <div className="relative z-20 max-w-xl text-center md:text-left md:mr-18">
+        <h1 className="text-4xl md:text-5xl font-bold text-white/90 mb-6">
+          We give your pet <br /> the care they deserve
         </h1>
         <p className="text-white/90 mb-6">
           We take good care of your pet with expert grooming, premium supplies,
+          <br />
           and lots of love. Because their well-being is our passion!
         </p>
         <SchedulingButton />
       </div>
 
-      {/* Imagem do cachorro */}
       <div className="relative mt-10 md:mt-0">
         <img
-          src={Cats}
-          alt="Dog in pajamas"
+          src={Pets}
+          alt="Dog and cat"
           className="w-[400px] md:w-[500px] z-10 relative"
         />
-
-        {/* Texto circular decorativo */}
-        <div className="absolute -top-10 -left-10 hidden md:block">
-          {/* Inserir SVG ou imagem do texto circular */}
-        </div>
-
-        {/* Imagem pequena decorativa */}
-        {/* <img
-          src={}
-          alt="Blurry"
-          className="absolute bottom-0 right-0 w-24 md:w-28 rotate-6"
-        /> */}
       </div>
-
-      {/* Patas no fundo */}
-      <div className="absolute inset-0 bg-[url('/paws.png')] bg-no-repeat bg-center opacity-10 pointer-events-none" />
     </section>
   );
 }
--- src/components/ServiceCard.tsx
+++ src/components/ServiceCard.tsx
@@ -6,8 +6,8 @@ type ServiceCardProps = {
 
 export function ServiceCard({ icon, title, description }: ServiceCardProps) {
   return (
-    <div className="flex flex-col items-center text-center px-6 py-4 group hover:bg-orange-50 rounded-xl transition-all">
-      <div className="bg-orange-300 w-16 h-16 rounded-2xl mb-4 flex items-center justify-center overflow-hidden mx-auto transform group-hover:-translate-y-2 transition-all duration-300">
+    <div className="flex flex-col items-center text-center px-6 py-4 group hover:bg-yellow-50 rounded-xl transition-all">
+      <div className="bg-[#f9d977] w-16 h-16 rounded-2xl mb-4 flex items-center justify-center overflow-hidden">
         <img src={icon} alt={title} className="w-10 h-10 object-cover" />
       </div>
       <h3 className="font-semibold text-lg mb-1">{title}</h3>
