e3221513b3a04c6a964b4b5dd8eb743b2204cf39
Maria Luiza <malumonteiro.dev@gmail.com>
2025-05-29 00:15:09 -0300

    refactor: enhance slideshow dark overlay

--- src/components/auth/Slideshow.tsx
+++ src/components/auth/Slideshow.tsx
@@ -31,7 +31,7 @@ export function Slideshow() {
       />
 
       {/* Dark Overlay */}
-      <div className="absolute inset-0 bg-black/50" />
+      <div className="absolute inset-0 bg-black/25" />
 
       {/* Left side with blur */}
       <div
