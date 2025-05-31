9973a49585206d719756baebf029e25777a2927d
Maria Luiza <malumonteiro.dev@gmail.com>
2025-05-31 00:40:34 -0300

    feat: changed hero background

--- src/components/home/Hero/ServiceCard.tsx
+++ src/components/home/Hero/ServiceCard.tsx
@@ -6,8 +6,8 @@ type ServiceCardProps = {
 
 export function ServiceCard({ icon, title, description }: ServiceCardProps) {
   return (
-    <div className="flex flex-col items-center text-center px-6 py-4 group hover:bg-yellow-50 rounded-xl transition-all">
-      <div className="bg-[#f9d977] w-16 h-16 rounded-2xl mb-4 flex items-center justify-center overflow-hidden transform group-hover:-translate-y-2 transition-transform duration-300 ease-out">
+    <div className="flex flex-col items-center text-center px-6 py-4 group hover:bg-amber-50 rounded-xl transition-all">
+      <div className="bg-amber-200 w-16 h-16 rounded-2xl mb-4 flex items-center justify-center overflow-hidden transform group-hover:-translate-y-2 transition-transform duration-300 ease-out">
         <img src={icon} alt={title} className="w-10 h-10 object-cover" />
       </div>
       <h3 className="font-semibold text-lg mb-1">{title}</h3>
